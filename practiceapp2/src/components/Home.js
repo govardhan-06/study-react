6298d8421904a04ae90a86e662cc54da65cbf9c4
govardhan-06 <156232211+govardhan-06@users.noreply.github.com>
2025-04-01 13:15:50 +0530

    Fixed React projects tracking issue

--- practiceapp2/src/components/Home.js
+++ practiceapp2/src/components/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-  return <h1>Welcome to Our Startup</h1>;
+  const navigate = useNavigate(); // Hook should be called here
+
+  const navigateClick = () => {
+    navigate("/about"); // Correct usage of navigate function
+  }
+
+  return (
+    <div>
+      <h1>Welcome to Our Startup</h1>
+      <button onClick={navigateClick}>Move to About</button>
+    </div>
+  );
 }
 
 export default Home;
--- practiceapp2/src/components/Navbar.js
+++ practiceapp2/src/components/Navbar.js
@@ -5,13 +5,13 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <nav className="navbar">
-      <NavLink to="/" className="nav-item" end>
+      <NavLink to="/" className={({ isActive }) => isActive ? "active-link" : ""}>
         Home
       </NavLink>
-      <NavLink to="/about" className="nav-item">
+      <NavLink to="/about" className={({ isActive }) => isActive ? "active-link" : ""}>
         About
       </NavLink>
-      <NavLink to="/contact" className="nav-item">
+      <NavLink to="/contact" className={({ isActive }) => isActive ? "active-link" : ""}>
         Contact
       </NavLink>
     </nav>
